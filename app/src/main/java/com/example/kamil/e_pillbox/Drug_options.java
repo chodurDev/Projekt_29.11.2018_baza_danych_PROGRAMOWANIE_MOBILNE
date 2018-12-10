9dcd3ff6b0485ec627b3367fece7f4df01ec3270
chodurDev <35374811+chodurDev@users.noreply.github.com>
2018-12-10 03:00:11 +0100

    kolejne usprawnienia

--- app/src/main/java/com/example/kamil/e_pillbox/Drug_options.java
+++ app/src/main/java/com/example/kamil/e_pillbox/Drug_options.java
@@ -1,15 +1,21 @@
 package com.example.kamil.e_pillbox;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.kamil.e_pillbox.DataAccess.LekarstwoDAO;
 import com.example.kamil.e_pillbox.pojo.Lekarstwo;
+
+import java.util.Calendar;
 
 public class Drug_options extends AppCompatActivity {
     EditText nazwa_leku;
@@ -20,18 +26,42 @@ public class Drug_options extends AppCompatActivity {
     private LekarstwoDAO lekDAO;
 
 
+    private int day,month,year;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drug_options);
 
-
+        final Calendar mcalendar=Calendar.getInstance();
 
 
         nazwa_leku=findViewById(R.id.editNazwaLeku);
         ilosc=findViewById(R.id.editIlosc);
         data_waznosci=findViewById(R.id.editDataWaznosci);
+        //wprowadzanie daty poprzez kalendarz
+        data_waznosci.setOnClickListener(mClickListener);
+        day=mcalendar.get(Calendar.DAY_OF_MONTH);
+        year=mcalendar.get(Calendar.YEAR);
+        month=mcalendar.get(Calendar.MONTH);
+
+//sluzy do ukrycia klawiatury podczas wprowadzania daty
+data_waznosci.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(data_waznosci.getWindowToken(),0);
+    }
+});
+
+
+
         button_dodaj_do_bazy=findViewById(R.id.button_dodaj_do_bazy);
         button_dodaj_do_bazy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +73,24 @@ public class Drug_options extends AppCompatActivity {
 
 
     }
+//wprowadzanie daty poprzez kalendarz
+    View.OnClickListener mClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            DateDialog();
+        }
+    };
+    public void DateDialog(){
+        DatePickerDialog.OnDateSetListener listener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+            {
+                data_waznosci.setText(dayOfMonth + "-" + monthOfYear + "-" + year);
+            }};
+        DatePickerDialog dpDialog=new DatePickerDialog(this, listener, year, month, day);
+        dpDialog.show();
+    }
+
 
     public void addLekDoBazy(View view){
         Lekarstwo lek=new Lekarstwo();
